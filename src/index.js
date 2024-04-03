2340a2bcf1be8480ccca88e8c90091d65cd9baa3
Devendra Dantal <devendra.dantal04@gmail.com>
2024-04-03 22:31:03 +0530

    Initial File Setup

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+import { app } from "./app.js";
+
+dotenv.config({
+    path: './.env'
+})
+
+const PORT = process.env.PORT || 5500;
+app.listen(PORT, (req, res) => {
+    console.log(`Server running on PORT http://localhost:${PORT}`);
+})
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+const asyncHandler = (func) => async (req, res, next) => {
+    try {
+        await func(req, res, next);
+    } catch (error) {
+        res.status(error.code || 500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+export { asyncHandler };
+
+// Using Promise and Resolve
+// const asyncHandler = (requestHandler) => {
+//     return (req, res, next) => {
+//         Promise.resolve(requestHandler(req, res, next))
+//             .catch((err) => next(err));
+//     }
+// }
